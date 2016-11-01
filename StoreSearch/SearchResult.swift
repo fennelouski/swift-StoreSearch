6e36736ffa2815f2d3aaf7084c696ae2dc5f2902
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-01 06:56:00 -0700

    add class for search result and display to screen

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Josiah Mory on 11/1/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
