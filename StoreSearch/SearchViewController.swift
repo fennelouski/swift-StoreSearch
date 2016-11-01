89a885e473a0674eb97cfb70f1e2da61268b3e91
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-10-31 17:08:47 -0700

    display dummy data in cells

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -13,6 +13,8 @@ class SearchViewController: UIViewController {
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var tableView: UITableView!
   
+  var searchResults: [String] = []
+  
   let topMarginForSearchBar: CGFloat = 64
   let noMargin: CGFloat = 0
 
@@ -34,6 +36,46 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    print("The search text is: '\(searchBar.text!)'")
+    searchResults = []
+    
+    searchBar.resignFirstResponder()
+    
+    for i in 0...2 {
+      searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+    }
+    
+    tableView.reloadData()
   }
 }
+
+// Mark: - DataSource
+extension SearchViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return searchResults.count
+  }
+ 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "SearchResultCell"
+    
+    var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+    
+    if cell == nil {
+      cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+    }
+    
+    cell.textLabel!.text = searchResults[indexPath.row]
+    return cell
+  }
+  
+}
+
+// Mark: - TableView Delegate
+extension SearchViewController: UITableViewDelegate {
+}
+
+
+
+
+
+
+
