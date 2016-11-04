90fbd9e76114b419d00d2acf581508c9c99eb0f3
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-04 07:19:00 -0700

    change search result properties to include additional data to be shown

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -11,4 +11,11 @@ import Foundation
 class SearchResult {
   var name = ""
   var artistName = ""
+  var artworkSmallUrl = ""
+  var artworkLargeUrl = ""
+  var storeUrl = ""
+  var kind = ""
+  var currency = ""
+  var price = 0.0
+  var genre = ""
 }
