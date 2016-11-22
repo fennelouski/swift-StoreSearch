3cc9a16e8db0e484d3fa2c6a7d7e02802c0ca084
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-22 14:47:34 -0800

    add new view controller for item details

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Josiah Mory on 11/22/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
