16b52f72ca69d55d9b0114801ec61bcb0394e7c0
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-22 15:15:42 -0800

    add presentation controller for detail view

---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,16 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Josiah Mory on 11/22/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+  override var shouldRemovePresentersView: Bool {
+    return false
+  }
+}
