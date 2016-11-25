dbfa989f064b818c3320a0a838bae067fdadc795
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-25 15:13:55 -0800

    add animation for detailView dismissed

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -125,6 +125,10 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
                            source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
     return BounceAnimationController()
   }
+  
+  func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return SlideOutAnimationController()
+  }
 }
 
 extension DetailViewController: UIGestureRecognizerDelegate {
---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,29 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Josiah Mory on 11/25/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0.3
+  }
+  
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    if let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from) {
+      let containerView = transitionContext.containerView
+      let duration = transitionDuration(using: transitionContext)
+      
+      UIView.animate(withDuration: duration, animations: {
+        fromView.center.y -= containerView.bounds.size.height
+        fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+      }, completion: { finished in
+        transitionContext.completeTransition(finished)
+      })
+    }
+  }
+}
