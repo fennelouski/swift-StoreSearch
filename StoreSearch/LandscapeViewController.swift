0f83c4296c8922e05948bf380f85402dcd463d4c
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-29 07:58:32 -0800

    add scroll view and page control to landscape view

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var pageControl: UIPageControl!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    view.removeConstraints(view.constraints)
+    view.translatesAutoresizingMaskIntoConstraints = true
+    
+    pageControl.removeConstraints(pageControl.constraints)
+    pageControl.translatesAutoresizingMaskIntoConstraints = true
+    
+    scrollView.removeConstraints(scrollView.constraints)
+    scrollView.translatesAutoresizingMaskIntoConstraints = true
+    
+    scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+  }
+  
+  override func viewWillLayoutSubviews() {
+   super.viewWillLayoutSubviews()
+    
+    scrollView.frame = view.bounds
+    
+    pageControl.frame = CGRect(x: 0,
+                               y: view.frame.size.height - pageControl.frame.size.height,
+                               width: view.frame.size.width,
+                               height: pageControl.frame.size.height)
   }
 
   
