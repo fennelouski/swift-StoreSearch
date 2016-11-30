7d26b9b85be6f5d63fbb7a66dbf0b77c57fed3eb
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-29 16:15:25 -0800

    add buttons based on search results

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -45,8 +45,82 @@ class LandscapeViewController: UIViewController {
                                y: view.frame.size.height - pageControl.frame.size.height,
                                width: view.frame.size.width,
                                height: pageControl.frame.size.height)
+    
+    if firstTime {
+      firstTime = false
+      tileButtons(searchResults)
+    }
   }
 
+  private func tileButtons(_ searchResults: [SearchResult]) {
+    var columnsPerPage = 5
+    var rowsPerPage = 3
+    var itemWidth: CGFloat = 96
+    var itemHeight: CGFloat = 88
+    var marginX: CGFloat = 0
+    var marginY: CGFloat = 20
+    var row = 0
+    var column = 0
+    var x = marginX
+    
+    let buttonWidth: CGFloat = 82
+    let buttonHeight: CGFloat = 82
+    let paddingHorz = (itemWidth - buttonWidth)/2
+    let paddingVert = (itemHeight - buttonHeight)/2
+    let buttonsPerPage = columnsPerPage * rowsPerPage
+    let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+    
+    let scrollViewWidth = scrollView.bounds.size.width
+    
+    switch scrollViewWidth {
+    case 568:
+      columnsPerPage = 6
+      itemWidth = 94
+      marginX = 2
+      
+    case 667:
+      columnsPerPage = 7
+      itemWidth = 95
+      itemHeight = 98
+      marginX = 1
+      marginY = 29
+      
+    case 736:
+      columnsPerPage = 8
+      rowsPerPage = 4
+      itemWidth = 92
+      
+    default:
+      break
+    }
+    
+    for (index, SearchResult) in searchResults.enumerated() {
+      let button = UIButton(type: .system)
+      button.backgroundColor = UIColor.white
+      button.setTitle("\(index)", for: .normal)
+      button.frame = CGRect(x: x + paddingHorz,
+                            y: marginY + CGFloat(row) * itemHeight + paddingVert,
+                            width: buttonWidth,
+                            height: buttonHeight)
+      
+      scrollView.addSubview(button)
+      row += 1
+      
+      if row == rowsPerPage {
+        row = 0
+        x += itemWidth; column += 1
+        if column == columnsPerPage {
+          column = 0
+          x += marginX * 2
+        }
+      }
+    }
+    
+    scrollView.contentSize = CGSize(width: CGFloat(numPages) * scrollViewWidth,
+                                    height: scrollView.bounds.size.height)
+    
+    print("Number of pages: \(numPages)")
+  }
   
   deinit {
     print("deinit \(self)")
