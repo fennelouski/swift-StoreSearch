3401e67467f3fe930d81068fa28b4956562544a8
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-12-03 14:56:44 -0800

    fix iphones so detail pop up still shows

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -70,6 +70,7 @@ class LandscapeViewController: UIViewController {
         
         let searchResult = list[(sender as! UIButton).tag - 2000]
         detailViewController.searchResult = searchResult
+        detailViewController.isPopUp = true
       }
     }
   }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -65,6 +65,7 @@ class SearchViewController: UIViewController {
         let indexPath = sender as! IndexPath
         let searchResult = list[indexPath.row]
         detailViewController.searchResult = searchResult
+        detailViewController.isPopUp = true
       }
     }
   }
