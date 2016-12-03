36ad287e9a4bbdb26e03ae729c4920df9311869f
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-12-03 14:34:52 -0800

    add split view functionality for iPad

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -12,9 +12,24 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  var splitViewController: UISplitViewController {
+    return window!.rootViewController as! UISplitViewController
+  }
+  var searchViewController: SearchViewController {
+    return splitViewController.viewControllers.first as! SearchViewController
+  }
+  var detailNavigationController: UINavigationController {
+    return splitViewController.viewControllers.last as! UINavigationController
+  }
+  var detailViewController: DetailViewController {
+    return detailNavigationController.topViewController as! DetailViewController
+  }
+  
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     customizeAppearance()
+    
+    detailViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
     return true
   }
 
